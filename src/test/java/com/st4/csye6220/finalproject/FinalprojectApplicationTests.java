a288dfec6157421e27936d0a0d596fdbf24fac0d
Jovin Nicholas <32580407+jovin-nicholas@users.noreply.github.com>
2024-04-24 19:39:05 -0400

    Initial commit
    Configuration files

---
+++ src/test/java/com/st4/csye6220/finalproject/FinalprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.st4.csye6220.finalproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
